f34ab93d82b15b12da25aac39435099351812682
Loan-Adrien <Loan-Adrien@exemple.com>
2023-12-05 15:38:30 +0100

    Hacakt

--- HackaWeb/src/Controller/HackatonController.php
+++ HackaWeb/src/Controller/HackatonController.php
@@ -5,14 +5,28 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Doctrine\Persistence\ManagerRegistry;
+
+use App\Entity\Hackaton;
 
 class HackatonController extends AbstractController
 {
     #[Route('/hackaton', name: 'app_hackaton')]
     public function index(): Response
     {
-        return $this->render('hackaton/index.html.twig', [
+        return $this->render('hackaton/hackatons.html.twig', [
             'controller_name' => 'HackatonController',
+        ]);
+    }
+
+    #[Route('/hackaton/{id}', name: 'app_hackaton_detail')]
+    public function detail($id, ManagerRegistry $doctrine): Response
+    {
+        $repository = $doctrine->getRepository(Hackaton::class);
+        $leHackaton = $repository->find($id);
+        return $this->render('hackaton/detail.html.twig', [
+            'controller_name' => 'HackatonController',
+            'unHackaton' => $leHackaton,
         ]);
     }
 }
